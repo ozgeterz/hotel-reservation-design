4dc6bf16f6ea63cd63814e6de9aaa39a6fe50dc7
ozgeterz <terziozge@hotmail.com>
2022-09-01 13:28:45 +0300

    Create Website Design

---
+++ src/components/MapComponent.vue
@@ -0,0 +1,290 @@
+<template>
+  <div class="turna">
+    <div class="turna-map">
+      <h2>Otele Ulaşım</h2>
+      <h5>
+        Antalya Şehir Merkezi, Sakıp Sabancı Bulvarı, Çimen Sokak,Antalya,
+        Turkey, 07050
+      </h5>
+      <iframe
+        class="turna-map-img"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3191.1479517700386!2d30.672536199999993!3d36.886808699999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14c390340cb47fb9%3A0x272477432af34686!2sMeltem%2C%20Sak%C4%B1p%20Sabanc%C4%B1%20Blv.%2C%2007030%20Muratpa%C5%9Fa%2FAntalya!5e0!3m2!1str!2str!4v1661869547301!5m2!1str!2str"
+        style="border: 0"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+      <button class="turna-map-buton">Haritada Göster</button>
+    </div>
+    <div class="turna-hotel">
+      <div class="turna-hotel-row">
+        <h2>Rixos Downtown Hotel (9.0)</h2>
+        <button class="turna-hotel-rezerv">Rezervasyon yap</button>
+      </div>
+      <img src="../İmage/3406339_810x458.jpg" />
+      <h5>
+        The purpose of lorem ipsum is to create a natural looking block of
+        text(sentence,paragraph,page,etc.) that doesn't distract from the
+        layout. A practice not without controversy,laying out pages with
+        meaningless filler text can be.
+      </h5>
+      <div class="turna-hotel-card-group">
+        <div class="turna-hotel-card">
+          <img
+            class="card-img-top"
+            width="250px"
+            height="290px"
+            src="../İmage/oda1.jpg"
+            alt="Card image cap"
+          />
+          <div class="turna-hotel-card-body">
+            <h3 class="card-title">Standart Oda</h3>
+            <div class="turna-features-column">
+              <img src="../İmage/check-circle.svg" />
+              <p>Banyo</p>
+            </div>
+            <div class="turna-features-column">
+              <img src="../İmage/check-circle.svg" />
+              <p>İnternet</p>
+            </div>
+            <div class="turna-features-column">
+              <img src="../İmage/check-circle.svg" />
+              <p>Havlu</p>
+            </div>
+            <div class="turna-hotel-card-info">
+              <h5>İki gece için toplam tutardır.</h5>
+              <button class="turna-hotel-card-info-room">Odanı Ayır</button>
+            </div>
+          </div>
+        </div>
+        <div class="turna-hotel-card">
+          <img
+            class="card-img-top"
+            width="250px"
+            height="290px"
+            src="../İmage/oda2.jpg"
+            alt="Card image cap"
+          />
+          <div class="turna-hotel-card-body">
+            <h3 class="card-title">İki Balkonlu Oda(Delux)</h3>
+            <div class="turna-features-column">
+              <img src="../İmage/check-circle.svg" />
+              <p>Banyo</p>
+            </div>
+            <div class="turna-features-column">
+              <img src="../İmage/check-circle.svg" />
+              <p>İnternet</p>
+            </div>
+            <div class="turna-features-column">
+              <img src="../İmage/check-circle.svg" />
+              <p>Havlu</p>
+            </div>
+            <div class="turna-hotel-card-info">
+              <h5>İki gece için toplam tutardır.</h5>
+              <button class="turna-hotel-card-info-room">Odanı Ayır</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="turna-features">
+        <h2>Otel Özellikleri</h2>
+        <div class="turna-features-card">
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Sauna</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Market</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Hamam</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Bar</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Masaj</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Açık Restoran</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Spa Merkezi</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Kapalı Restoran</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Otelde Otopark</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Tv Salonu</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Engelli Odaları</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Araç Kiralama</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Havaalanı</p>
+          </div>
+        </div>
+      </div>
+      <div class="turna-features">
+        <h2>Otel Koşulları</h2>
+        <div class="turna-features-card">
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Sauna</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Market</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Hamam</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Bar</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Masaj</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Açık Restoran</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Spa Merkezi</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Kapalı Restoran</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Otelde Otopark</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Tv Salonu</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Engelli Odaları</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Araç Kiralama</p>
+          </div>
+          <div class="turna-features-column">
+            <img src="../İmage/check.svg" />
+            <p>Havaalanı</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MapComponent",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/color.scss";
+.turna {
+  display: flex;
+  align-items: flex-start;
+  text-align: start;
+  max-width: 960px;
+  padding-inline-start: 10%;
+  gap: 25px;
+  &-map {
+    &-buton {
+      padding: 10px 90px;
+      background: $butonbackground1;
+      cursor: pointer;
+      border-radius: 8px;
+      margin-top: 10px;
+      color: $butoncolor1;
+      font-weight: bold;
+      border: 1px solid $butoncolor1;
+    }
+    &-img {
+      width: 270px;
+      height: 180px;
+    }
+  }
+
+  &-hotel {
+    &-row {
+      display: flex;
+      justify-content: space-between;
+    }
+
+    &-card {
+      border: 2px solid $bordercolor1;
+      display: flex;
+      margin-top: 20px;
+      &-info {
+        border-top: 2px solid $bordercolor2;
+        display: flex;
+        gap: 180px;
+        &-room {
+          background-color: $butonbackground2;
+          color: $maincolor;
+          border: none;
+          border-radius: 8px;
+          padding: 0px 50px;
+          margin-bottom: 10px;
+          cursor: pointer;
+        }
+      }
+      &-body {
+        padding-inline-start: 2%;
+      }
+    }
+    &-rezerv {
+      background-color: $butonbackground2;
+      color: $maincolor;
+      border: none;
+      border-radius: 8px;
+      padding: 0px 50px;
+      margin-bottom: 10px;
+      cursor: pointer;
+    }
+  }
+  &-features {
+    border-top: 2px solid $bordercolor2;
+    margin-top: 20px;
+
+    &-card {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+    }
+    &-column {
+      display: flex;
+    }
+  }
+}
+</style>
